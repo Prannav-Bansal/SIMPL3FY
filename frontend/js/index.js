8d303f3340389dd7d548bb71c1d562f472839d79
Pranav Bansal <105575423+Prannav-Bansal@users.noreply.github.com>
2025-02-13 20:18:39 +0530

    Add files via upload

---
+++ frontend/js/index.js
@@ -0,0 +1,14 @@
+// Typing text animation script
+var typed1 = new Typed(".typing", {
+    strings: ["Set Your Budget Easily.", "Add Your Expenses Hassle -Free.", "View Your Expenses.", "Get Financial Advice.", "Talk To Our Personal Chat Bot.", "Calculate Your Monthly EMI Easily."],
+    typeSpeed: 30,
+    backSpeed: 20,
+    loop: true
+});
+
+var typed2 = new Typed(".typing-2", {
+    strings: ["Set Your Budget Easily.", "Add Your Expenses Hassle -Free.", "View Your Expenses.", "Get Financial Advice. ", "Talk To Our Personal Chat Bot.", "Calculate Your Monthly EMI Easily. "],
+    typeSpeed: 30,
+    backSpeed: 20,
+    loop: true
+});
